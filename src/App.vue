0b40ee732f96b17233a6e9c9efa01b2729e35ebc
shafiulsaurav <shafiulsaurav8@gmail.com>
2023-10-29 19:38:25 +0600

    Bootstrap-Installation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="text-success">Hello World</h2>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// Import Custom CSS
+import './assets/sass/style.scss'
+
 const app = createApp(App)
-
 app.use(createPinia())
-
 app.mount('#app')
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
